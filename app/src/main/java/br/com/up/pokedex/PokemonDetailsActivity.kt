02b42456e83d50f99d844d6b7395fe163896594c
Matheus-Lara <matheusodelara@gmail.com>
2022-11-22 23:03:54 -0300

    feat: loads pokemon image

--- app/src/main/java/br/com/up/pokedex/PokemonDetailsActivity.kt
+++ app/src/main/java/br/com/up/pokedex/PokemonDetailsActivity.kt
@@ -15,12 +15,11 @@ class PokemonDetailsActivity : AppCompatActivity() {
         val pokemon = intent.getStringExtra("pokemon")
         PokeApi().getPokemonByName(pokemon!!) { pokemon ->
 
-            if(pokemon != null){
+            if (pokemon != null) {
                 val id = pokemon.id;
-
-//                val url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$id.png"
-//                val imageView : ImageView = findViewById(R.id.poke_image)
-//                Picasso.get().load(url).into(imageView)
+                val url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$id.png"
+                val imageView : ImageView = findViewById(R.id.pokemon_image)
+                Picasso.get().load(url).into(imageView)
             }
         }
 
