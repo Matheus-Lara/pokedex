958d9182ca79caa715bda8e5741d5f8e9759b473
Matheus-Lara <matheusodelara@gmail.com>
2022-11-22 22:52:20 -0300

    feat: adds other pokemon data models

--- app/src/main/java/br/com/up/pokedex/PokemonDetailsActivity.kt
+++ app/src/main/java/br/com/up/pokedex/PokemonDetailsActivity.kt
@@ -2,7 +2,9 @@ package br.com.up.pokedex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import br.com.up.pokedex.network.PokeApi
+import com.squareup.picasso.Picasso
 
 class PokemonDetailsActivity : AppCompatActivity() {
 
@@ -11,12 +13,14 @@ class PokemonDetailsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_pokemon_details)
 
         val pokemon = intent.getStringExtra("pokemon")
-
-        PokeApi().getPokemonByName(pokemon!!){
-                pokemon ->
+        PokeApi().getPokemonByName(pokemon!!) { pokemon ->
 
             if(pokemon != null){
+                val id = pokemon.id;
 
+//                val url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$id.png"
+//                val imageView : ImageView = findViewById(R.id.poke_image)
+//                Picasso.get().load(url).into(imageView)
             }
         }
 
--- app/src/main/java/br/com/up/pokedex/model/Pokemon.kt
+++ app/src/main/java/br/com/up/pokedex/model/Pokemon.kt
@@ -1,6 +1,11 @@
 package br.com.up.pokedex.model
 
 data class Pokemon(
-    val url : String,
-    val name : String
+    val id: Int,
+    val url: String,
+    val name: String,
+    val types: List<Type>,
+    val stats: List<Stat>,
+    val abilities: List<Ability>,
+    val moves: List<Move>
 )
